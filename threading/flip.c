c528804f0f6922f96b5d262b5818ada61509e8aa
Thanh Loic Nguyen <loic.nguyen@epfl.ch>
2017-09-27 00:04:56 +0200

    version without thread working

--- threading/flip.c
+++ threading/flip.c
@@ -21,13 +21,45 @@
 #include "uint128.h"
 #include "flip.h"
 
+
+
+
+// create a bitmask where bit at position n is set
+#define BITMASK(n)          (((uint128_t) 1) << (n))
+
+// check if bit n in v is set
+#define BIT_IS_SET(v,n)     (((v) & BITMASK(n)) == BITMASK(n))
+
+// flip bit n in v
+#define BIT_FLIP(v,n)        ((v) =  (v) ^  BITMASK(n))
+
+// clear bit n in v
+#define BIT_CLEAR(v,n)      ((v) =  (v) & ~BITMASK(n))
+
+// declare a mutex, and it is initialized as well
+//static pthread_mutex_t      mutex          = PTHREAD_MUTEX_INITIALIZER;
+
+/*-------------------------------------------------------------------------*/
+
+
 int main (void)
 {
     // TODO: start threads to flip the pieces and output the results
     // (see thread_malloc_free_test() and thread_mutex_test() how to use threads and mutexes,
     //  see bit_test() how to manipulate bits in a large integer)
-
-
+    
+    
+    for (int i = 1; i < NROF_PIECES; i++){
+		for(int j = i; j < NROF_PIECES; j = j + i){
+			BIT_FLIP(buffer[j/128], (j%128));
+		}
+	}
+	
+	for (int i = 0; i < NROF_PIECES; i++){
+		if (BIT_IS_SET(buffer[i/128], (i%128))) {
+			printf("%d\n", i);
+		}
+	}
     return (0);
 }
 
